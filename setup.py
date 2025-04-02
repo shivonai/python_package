657664bc7c5d609660989d240bdc4b812a9eba1b
Vraj Patel <vrajh.patel531@gmail.com>
2025-04-01 22:40:34 -0400

    added requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="shivonai",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "requests>=2.25.0",
-    ],
+    install_requires=requirements,
     extras_require={
         "langchain": ["langchain>=0.1.0"],
         "llamaindex": ["llama-index>=0.1.0"],
